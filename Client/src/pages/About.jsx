4e6fd249827192ee9c84c703e830fcdec9903588
Arijit <arijitbasubd@gmail.com>
2025-05-18 00:26:03 +0600

    update

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ColorGrid from "../Components/ColorGrid";
 
 const About = () => {
   return (
     <div className="text-4xl font-bold text-center bg-yellow py-5">
       About Page
+      <ColorGrid/>
     </div>
   );
 };
